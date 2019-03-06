122b89aa8f66a002a1bac26c5a2b61fb1daf957c
veres roland <rolandveres@yahoo.com>
2019-03-06 16:04:24 +0200

    sdfsdfsdfsd

---
+++ Desenare/Desenare/DrawingEngine.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desenare
+{
+    class DrawingEngine
+    {
+        private PictureBox box;
+        private Bitmap map;
+        private Graphics graphicsObj;
+        public PictureBox Box
+        {
+            get { return box; }
+        }
+        public  Bitmap Map
+        {
+            get { return map; }
+
+        }
+        public SolidBrush SBrush
+        {
+            get;set;
+        }
+        public Pen P
+        {
+            get;set;
+        }
+        public  Graphics GraphicsObj
+        {
+            get { return graphicsObj; }
+        }
+    
+        public DrawingEngine(PictureBox  box)
+        {
+            this.box = box;
+            map = new Bitmap(box.Height, box.Width);
+            graphicsObj = Graphics.FromImage(map);
+           
+        }
+        public void Refresh()
+        {
+            box.Image = map;
+        }
+
+    }
+}
--- Desenare/Desenare/Form1.cs
+++ Desenare/Desenare/Form1.cs
@@ -16,5 +16,51 @@ namespace Desenare
         {
             InitializeComponent();
         }
+        DrawingEngine drawing;
+        Label[] lb;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            drawing = new DrawingEngine(pictureBox);
+            drawing.P = new Pen(Color.Black, 10);
+            //desenare linie
+            drawing.GraphicsObj.DrawLine(drawing.P, 100, 200, 300, 400);
+           
+
+            //desenare Polygon
+            Point[] p = new Point[] { new Point(30, 50), new Point(60, 70), new Point(100, 23) };
+            drawing.GraphicsObj.DrawPolygon(drawing.P, p);
+
+            //desenare cerc??
+
+            drawing.GraphicsObj.DrawEllipse(drawing.P, 345, 234, 50, 100);
+            //umplere cerc
+
+            SolidBrush sB = new SolidBrush(Color.LightGreen);
+            drawing.GraphicsObj.FillEllipse(sB, 345, 234, 50, 100);
+            //umplere polygon 
+
+            drawing.GraphicsObj.FillPolygon(sB, p);
+
+
+            string tags = "ABC";
+             lb = new Label[3];
+            for(int i=0;i<p.Length;i++)
+            {
+                 lb[i] = new Label();
+                lb[i].Text = tags[i].ToString();
+                lb[i].BackColor = Color.White;
+                lb[i].ForeColor = Color.Black;
+                lb[i].Location = new Point(p[i].X - 20, p[i].Y);
+                lb[i].Size = new Size(20, 30);
+                lb[i].Font = new Font(FontFamily.GenericMonospace, 3);
+                
+                this.Controls.Add(lb[i]);
+              
+              
+            }
+            drawing.Refresh();
+
+
+        }
     }
 }
